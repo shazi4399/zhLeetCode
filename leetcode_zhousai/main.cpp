99c4d4c4b908c4ffc861f6fa87c815eb37440b08
zhanghao <2573344091@qq.com>
2022-03-13 11:20:57 +0800

    Initial Commit

---
+++ leetcode_zhousai/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leetcode_zhousai
+//
+//  Created by zhanghao on 2022/3/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
