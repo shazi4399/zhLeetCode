cc12d42ea5337d7f81ae1fbd483c18b0e33fa89c
zhanghao <2573344091@qq.com>
2022-03-27 21:17:53 +0800

    add 5268

---
+++ 5268/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  5268
+//
+//  Created by zhanghao on 2022/3/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
