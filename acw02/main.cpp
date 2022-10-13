9de1a9fda49ea0845dd0f342bc2151782658febb
zhanghao <2573344091@qq.com>
2022-10-13 09:39:15 +0800

    fix 0/1背包问题

--- acw02/main.cpp
+++ acw02/main.cpp
@@ -3,21 +3,24 @@
 //
 #include <iostream>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 
 const int N = 1010;
 
 int n,m;
 int v[N],w[N];
-int f[N][N];
+//vector<vector<int>>f(N,vector<int>(N,0));
+//int f[N][N];
 
 int main(){
-    cin >> n >> m;
-    for(int i = 0 ; i <= n; i++) cin >> v[i] >> w[i];
+
+    cin >> n >> m;//4 5  4个物品 背包容量为5
+    vector<vector<int>>f(n+1,vector<int>(m+1,0));
+    for(int i = 1 ; i <= n; i++) cin >> v[i] >> w[i];
 
     for(int i = 1 ; i <= n; i++)
-        for(int j = 0 ; j <= m; j++){
+        for(int j = 1 ; j <= m; j++){
             f[i][j] = f[i-1][j];
             if(j >= v[i])
                 f[i][j] = max(f[i][j],f[i-1][j-v[i]]+w[i]);
@@ -27,8 +30,5 @@ int main(){
 }
 
 
-int dp(){
-
-}
 
 
